f3b491a951f5caf5ade0cbd5e9b5fd8a2ff883cd
Grigorios Papadopoulos <pagr1011@h-ka.de>
2023-05-10 11:38:07 +0200

    test anpassung

--- __tests__/login.ts
+++ __tests__/login.ts
@@ -2,7 +2,7 @@ import { type AxiosInstance, type AxiosResponse } from 'axios';
 import {
     type GraphQLQuery,
     type GraphQLResponseBody,
-} from './buch/buch-query.resolver.test.js';
+} from './comicheft/comicheft-query.resolver.test.js';
 import { httpsAgent, loginPath } from './testserver.js';
 import { type LoginResult } from '../src/security/auth/service/auth.service.js';
 import dotenv from 'dotenv';
