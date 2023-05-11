39328bdcd6ad9873c5cdefa67b7b020a6fcf1b3a
fued1011 <fued1011@h-ka.de>
2023-05-11 17:46:39 +0200

    fehlerbehebung

--- __tests__/comicheft/comicheft-GET.controller.test.ts
+++ __tests__/comicheft/comicheft-GET.controller.test.ts
@@ -42,7 +42,7 @@ describe('GET /rest', () => {
 
     test('Alle Comichefte', async () => {
         // when
-        const response: AxiosResponse<ComichefteModel> = await client.get('/');
+        const response: AxiosResponse<ComichefteModel> = await client.get('');
 
         // then
         const { status, headers, data } = response;
@@ -57,7 +57,7 @@ describe('GET /rest', () => {
             .map((comicheft) => comicheft._links.self.href)
             .forEach((selfLink) => {
                 // eslint-disable-next-line security/detect-non-literal-regexp, security-node/non-literal-reg-expr
-                expect(selfLink).toMatch(new RegExp(`^${baseURL}`, 'u'));
+                expect(selfLink).toMatch(new RegExp(`^${baseURL}`, 'iu'));
             });
     });
 
